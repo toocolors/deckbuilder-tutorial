2027d816987e3b927288e408ca160774e5666a59
toocolors <noa.algorithms@gmail.com>
2026-06-29 07:00:21 -0700

    Add PackedScene field for card scene

--- scripts/deck_n_hand.gd
+++ scripts/deck_n_hand.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var card_scene: PackedScene = preload("res://scenes/card.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
