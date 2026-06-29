e5849d008135e61a4b5dadd0a97dc7589b3ce443
toocolors <noa.algorithms@gmail.com>
2026-06-29 06:38:35 -0700

    Add class_name to card script, add node reference variables
    Also change names of label nodes

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,14 +1,13 @@
-extends Node2D
+class_name Card extends Node2D
 
 @export var  cardName: String = "Card Name"
 @export var cardDescription: String = "Card Description"
 @export var cardCost: int = 1
 @export var cardImage: Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var costLabel: Label = $CostDisplay/CostLabel
+@onready var descLabe: Label = $CardDescription
+@onready var nameLabel: Label = $CardName/NameLabel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
