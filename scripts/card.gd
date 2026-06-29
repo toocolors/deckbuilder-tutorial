d46c9f2648e817d2e5ddf9eaefe54fc18200548d
toocolors <noa.algorithms@gmail.com>
2026-06-29 06:43:32 -0700

    Change variables to snake case

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,13 +1,18 @@
 class_name Card extends Node2D
 
-@export var  cardName: String = "Card Name"
-@export var cardDescription: String = "Card Description"
-@export var cardCost: int = 1
-@export var cardImage: Node2D
+@export var  card_name: String = "Card Name"
+@export var card_desc: String = "Card Description"
+@export var card_cost: int = 1
+@export var card_image: Node2D
 
-@onready var costLabel: Label = $CostDisplay/CostLabel
-@onready var descLabe: Label = $CardDescription
-@onready var nameLabel: Label = $CardName/NameLabel
+@onready var cost_label: Label = $CostDisplay/CostLabel
+@onready var desc_label: Label = $CardDescription
+@onready var name_label: Label = $CardName/NameLabel
+
+func _ready():
+	cost_label.set_text(str(card_cost))
+	desc_label.set_text(card_desc)
+	name_label.set_text(card_name)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
