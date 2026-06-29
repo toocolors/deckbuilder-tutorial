812cef05c0ac6519867ff7b82c6e2506c45f3881
toocolors <noa.algorithms@gmail.com>
2026-06-29 10:00:29 -0700

    Create attack and defend cards with buttons

--- scripts/deck_n_hand.gd
+++ scripts/deck_n_hand.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@onready var card_scene: PackedScene = preload("res://scenes/card.tscn")
+@onready var attack_card_scene: PackedScene = preload("res://scenes/cards/attack_card.tscn")
+@onready var defend_card_scene: PackedScene = preload("res://scenes/cards/defend_card.tscn")
 
 @onready var spawn_point = $CanvasLayer/Spawn
 
@@ -15,14 +16,10 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	var card = card_scene.instantiate()
+	var card = attack_card_scene.instantiate()
 	spawn_point.add_child(card)
-	card.set_values(1, "First Card Description", "Card One")
-	card.visible = true
 
 
 func _on_button_2_pressed() -> void:
-	var card = card_scene.instantiate()
+	var card = defend_card_scene.instantiate()
 	spawn_point.add_child(card)
-	card.set_values(2, "Second Card Description", "Card Two")
-	card.visible = true
