ed17ade8c8e4d5bafe5a164ac306745c804b79a4
toocolors <noa.algorithms@gmail.com>
2026-06-29 11:08:17 -0700

    Card rotates and moves in hand

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -2,8 +2,10 @@
 extends Node2D
 
 @export var hand_radius: int = 100
+@export var card_angle: float = 90
 
 @onready var collision_shape: CollisionShape2D = $DebugShape
+@onready var test_card = $AttackCard
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +15,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if (collision_shape.shape as CircleShape2D).radius != hand_radius:
 		(collision_shape.shape as CircleShape2D).set_radius(hand_radius)
+	
+	test_card.set_position(get_card_position(card_angle))
+	test_card.set_rotation(deg_to_rad(card_angle + 90))
 
-func get_card_position(angle:float):
-	pass
+func get_card_position(angle_in_deg:float) -> Vector2:
+	var x: float = hand_radius * cos(deg_to_rad(angle_in_deg))
+	var y: float = hand_radius * sin(deg_to_rad(angle_in_deg))
+	return Vector2(x, y)
