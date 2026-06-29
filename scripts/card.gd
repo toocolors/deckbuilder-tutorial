05ed246e38c7ec6607bd13d58c7323914094a343
toocolors <noa.algorithms@gmail.com>
2026-06-29 06:31:41 -0700

    Add name, description, cost, image fields

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@export var  cardName: String = "Card Name"
+@export var cardDescription: String = "Card Description"
+@export var cardCost: int = 1
+@export var cardImage: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
