476c661211c952e1812bd3fb5e862c6d1edf812d
toocolors <noa.algorithms@gmail.com>
2026-06-29 07:15:36 -0700

    Add spawn reference field, create card in on_button_pressed

--- scripts/deck_n_hand.gd
+++ scripts/deck_n_hand.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var card_scene: PackedScene = preload("res://scenes/card.tscn")
 
+@onready var spawn_point = $CanvasLayer/Spawn
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +15,7 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	pass
+	var card = card_scene.instantiate()
+	spawn_point.add_child(card)
+	card.set_values(3, "Card Description", "New Card")
+	card.visible = true
